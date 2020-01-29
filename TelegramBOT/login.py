ef655786a17d81777bd85a4f4a998d15bc7436a6
malinamedusa <malinamedusa@gmail.com>
2020-01-29 13:44:30 +0300

    Update login.py

--- TelegramBOT/login.py
+++ TelegramBOT/login.py
@@ -2,9 +2,9 @@ import pyowm
 import requests
 import telebot
 
-token_bot = "1031293192:AAGoYUaXkJOIWX1jB_U1e6gilchmGkbEJ8w"
+token_bot = "***"
 BASE_URL = 'https://api.telegram.org/bot<token>/'
-token_owm = 'd9d23e920d8ad8c8bb66d92bb7e9f0ac'
+token_owm = '***'
 
 
 bot = telebot.TeleBot(token_bot)
